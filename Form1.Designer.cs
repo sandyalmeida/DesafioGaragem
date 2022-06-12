8e41f693fd3f48962b58f4e5a85f9073d4283168
sandyalmeida <sandyalmeida20@hotmail.com>
2022-06-12 19:48:33 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WinFormsAppDesafioGaragem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lstSaida = new System.Windows.Forms.ListBox();
+            this.btnSaida = new System.Windows.Forms.Button();
+            this.txtPlacaSaida = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker6 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.lstEntrada = new System.Windows.Forms.ListBox();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.txtPlacaEntrada = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tabPage2.Controls.Add(this.dateTimePicker2);
+            this.tabPage2.Controls.Add(this.dateTimePicker1);
+            this.tabPage2.Controls.Add(this.lstSaida);
+            this.tabPage2.Controls.Add(this.btnSaida);
+            this.tabPage2.Controls.Add(this.txtPlacaSaida);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tabPage2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(355, 230);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Saída de Veículos";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Enabled = false;
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(269, 48);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker2.TabIndex = 16;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(269, 26);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // lstSaida
+            // 
+            this.lstSaida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lstSaida.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstSaida.FormattingEnabled = true;
+            this.lstSaida.ItemHeight = 15;
+            this.lstSaida.Location = new System.Drawing.Point(6, 97);
+            this.lstSaida.Name = "lstSaida";
+            this.lstSaida.Size = new System.Drawing.Size(343, 124);
+            this.lstSaida.TabIndex = 14;
+            // 
+            // btnSaida
+            // 
+            this.btnSaida.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSaida.Location = new System.Drawing.Point(97, 71);
+            this.btnSaida.Name = "btnSaida";
+            this.btnSaida.Size = new System.Drawing.Size(143, 23);
+            this.btnSaida.TabIndex = 11;
+            this.btnSaida.Text = "Saída de Veículo";
+            this.btnSaida.UseVisualStyleBackColor = true;
+            this.btnSaida.Click += new System.EventHandler(this.btnSaida_Click);
+            // 
+            // txtPlacaSaida
+            // 
+            this.txtPlacaSaida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtPlacaSaida.Location = new System.Drawing.Point(6, 26);
+            this.txtPlacaSaida.MaxLength = 7;
+            this.txtPlacaSaida.Name = "txtPlacaSaida";
+            this.txtPlacaSaida.Size = new System.Drawing.Size(153, 23);
+            this.txtPlacaSaida.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Assertive;
+            this.label4.Location = new System.Drawing.Point(56, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Placa";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tabPage1.Controls.Add(this.dateTimePicker5);
+            this.tabPage1.Controls.Add(this.dateTimePicker6);
+            this.tabPage1.Controls.Add(this.dateTimePicker4);
+            this.tabPage1.Controls.Add(this.dateTimePicker3);
+            this.tabPage1.Controls.Add(this.lstEntrada);
+            this.tabPage1.Controls.Add(this.btnEntrada);
+            this.tabPage1.Controls.Add(this.txtPlacaEntrada);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(355, 230);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Entrada de Veículos";
+            // 
+            // dateTimePicker5
+            // 
+            this.dateTimePicker5.Enabled = false;
+            this.dateTimePicker5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker5.Location = new System.Drawing.Point(266, 47);
+            this.dateTimePicker5.Name = "dateTimePicker5";
+            this.dateTimePicker5.Size = new System.Drawing.Size(83, 23);
+            this.dateTimePicker5.TabIndex = 18;
+            // 
+            // dateTimePicker6
+            // 
+            this.dateTimePicker6.Enabled = false;
+            this.dateTimePicker6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker6.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker6.Location = new System.Drawing.Point(266, 25);
+            this.dateTimePicker6.Name = "dateTimePicker6";
+            this.dateTimePicker6.Size = new System.Drawing.Size(83, 23);
+            this.dateTimePicker6.TabIndex = 17;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.CalendarFont = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker4.Enabled = false;
+            this.dateTimePicker4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(272, 256);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker4.TabIndex = 8;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.CalendarFont = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker3.Enabled = false;
+            this.dateTimePicker3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker3.Location = new System.Drawing.Point(272, 232);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker3.TabIndex = 7;
+            // 
+            // lstEntrada
+            // 
+            this.lstEntrada.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstEntrada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lstEntrada.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstEntrada.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstEntrada.ForeColor = System.Drawing.Color.Black;
+            this.lstEntrada.FormattingEnabled = true;
+            this.lstEntrada.ItemHeight = 15;
+            this.lstEntrada.Location = new System.Drawing.Point(6, 97);
+            this.lstEntrada.Name = "lstEntrada";
+            this.lstEntrada.Size = new System.Drawing.Size(343, 120);
+            this.lstEntrada.TabIndex = 6;
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnEntrada.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEntrada.Location = new System.Drawing.Point(97, 71);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(152, 23);
+            this.btnEntrada.TabIndex = 5;
+            this.btnEntrada.Text = "Cadastro de Veículo";
+            this.btnEntrada.UseVisualStyleBackColor = false;
+            this.btnEntrada.Click += new System.EventHandler(this.btnEntrada_Click);
+            // 
+            // txtPlacaEntrada
+            // 
+            this.txtPlacaEntrada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtPlacaEntrada.Location = new System.Drawing.Point(6, 26);
+            this.txtPlacaEntrada.MaxLength = 7;
+            this.txtPlacaEntrada.Name = "txtPlacaEntrada";
+            this.txtPlacaEntrada.ShortcutsEnabled = false;
+            this.txtPlacaEntrada.Size = new System.Drawing.Size(153, 22);
+            this.txtPlacaEntrada.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(56, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Placa";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(363, 258);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 257);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Park S&A";
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TextBox txtPlacaEntrada;
+        private Label label1;
+        private TabPage tabPage2;
+        private Button btnEntrada;
+        //private ListBox listaEntrada;
+        private Button btnSaida;
+        private TextBox txtPlacaSaida;
+        private Label label4;
+        private ListBox lstEntrada;
+        private ListBox lstSaida;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker4;
+        private DateTimePicker dateTimePicker3;
+        private DateTimePicker dateTimePicker5;
+        private DateTimePicker dateTimePicker6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+namespace WinFormsAppDesafioGaragem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Persistencia.lerArquivoEntrada(listaEntrada);
+            Persistencia.lerArquivoSaida(listaSaida);
+            preenchelstEntrada();
+            preenchelstSaida();
+        }
+
+        List<Veiculo> listaEntrada = new List<Veiculo>();
+        List<Veiculo> listaSaida = new List<Veiculo>();
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void preenchelstEntrada()
+        {
+            //limpa a lista de entrada
+            lstEntrada.Items.Clear();
+
+            //percorre a lista entrada e preenche o listbox
+            foreach (var item in listaEntrada)
+            {
+                lstEntrada.Items.Add(item.Placa + " - " + item.DataEntrada + " - " + item.HoraEntrada);
+            }
+        }
+
+        private void preenchelstSaida()
+        {
+            //limpa a lista de Saida
+            //lstSaida.Items.Clear();
+
+            //percorre a lista Saida e preenche o listbox
+            foreach (var item in listaSaida)
+            {
+                lstSaida.Items.Add(item.Placa + " - " + item.TempoPermanecia + " minuto(s) - R$ " + item.ValorCobrado);
+            }
+        }
+        private bool pesquisaVeiculo(string placa)
+        {
+            foreach (var veiculo in listaEntrada)
+            {
+                if (placa == veiculo.Placa)
+                    return true;
+            }
+            return false;
+        }
+
+        private void btnEntrada_Click(object sender, EventArgs e)
+        {
+            Veiculo veiculo = new Veiculo(txtPlacaEntrada.Text);
+            txtPlacaEntrada.Text = "";
+            //gerarDataHora(string tipo)
+            //pesquisa se a placa ja existe na entrada
+            var existe = pesquisaVeiculo(veiculo.Placa);
+            if (existe)
+            {
+                MessageBox.Show("A placa ja existe!");
+                return;
+            }
+            veiculo.gerarDataHora("entrada");
+            if (!Veiculo.temLugar(listaEntrada,50))
+            {
+                MessageBox.Show("Garagem cheia");
+                return;
+            }
+            // adiciona o objeto veiculo na lista de veiculos
+            listaEntrada.Add(veiculo);
+           //preenche a lista de entrada
+            preenchelstEntrada();
+
+            Persistencia.gravarNoArquivoEntrada(listaEntrada);
+
+        }
+
+        private void btnSaida_Click(object sender, EventArgs e)
+        {
+            var posicao = Veiculo.localizado(txtPlacaSaida.Text, listaEntrada);
+            txtPlacaSaida.Text = "";
+            Veiculo veiculo = listaEntrada[posicao];
+
+            veiculo.gerarDataHora("saida");
+
+            veiculo.realizarCobranca(5);
+
+            MessageBox.Show("Valor cobrado: "+veiculo.ValorCobrado);
+
+            listaEntrada.Remove(veiculo);
+
+            preenchelstEntrada();
+
+            listaSaida.Add(veiculo);
+
+
+            lstSaida.Items.Add(veiculo.Placa 
+                + " - " + veiculo.TempoPermanecia
+                + " minuto(s) - R$ " + veiculo.ValorCobrado);
+
+            Persistencia.gravarNoArquivoSaida(listaSaida);
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
